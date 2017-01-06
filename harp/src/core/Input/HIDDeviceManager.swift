5aa00fadec140aed8b3daf7c8eada9d66824d721
gomachan_7 <tt@a-azarashi.jp>
2017-01-07 02:46:46 +0900

    :recycle: Refactor: Add comments and name constants

--- harp/src/core/Input/HIDDeviceManager.swift
+++ harp/src/core/Input/HIDDeviceManager.swift
@@ -3,11 +3,13 @@ import IOKit
 import IOKit.hid
 import RxSwift
 
+// Define matching conditions for Joypads used on this app
 fileprivate let criteria: NSDictionary = [
   kIOHIDDeviceUsagePageKey: kHIDPage_GenericDesktop,
   kIOHIDDeviceUsageKey: kHIDUsage_GD_Joystick
 ]
 
+// Define swift ref <-> pointer converter (working with c++ functions)
 fileprivate func bridge<T: AnyObject>(obj: T) -> UnsafeMutableRawPointer {
   return Unmanaged.passUnretained(obj).toOpaque()
 }
@@ -16,28 +18,37 @@ fileprivate func bridge<T: AnyObject>(ptr: UnsafeRawPointer) -> T {
   return Unmanaged<T>.fromOpaque(ptr).takeUnretainedValue()
 }
 
+// Define joypad input to JoypadInputType converter
+fileprivate struct Input {
+  static let on = 1
+  static let off = 0
+  static let axisPlus = 0
+  static let axisNuetral = 127
+  static let axisMinus = 255
+}
+
 fileprivate let inputValueMap: [Int:[Int:[Int:JoypadInputType]]] = [
   kHIDPage_Button: [
-    1: [0: .buttonOff1, 1: .buttonOn1], // kHIDUsage_Button_1,
-    2: [0: .buttonOff2, 1: .buttonOn2], // kHIDUsage_Button_2,
-    3: [0: .buttonOff3, 1: .buttonOn3], // kHIDUsage_Button_3,
-    4: [0: .buttonOff4, 1: .buttonOn4], // kHIDUsage_Button_4 exist but
-    5: [0: .buttonOff5, 1: .buttonOn5], // kHIDUsage_Button_5 do not exist, so use Raw Int
-    6: [0: .buttonOff6, 1: .buttonOn6],
-    7: [0: .buttonOff7, 1: .buttonOn7],
-    8: [0: .buttonOff8, 1: .buttonOn8],
-    9: [0: .buttonOff9, 1: .buttonOn9],
-    10: [0: .buttonOff10, 1: .buttonOn10],
-    11: [0: .buttonOff11, 1: .buttonOn11],
-    12: [0: .buttonOff12, 1: .buttonOn12],
-    13: [0: .buttonOff13, 1: .buttonOn13],
-    14: [0: .buttonOff14, 1: .buttonOn14],
-    15: [0: .buttonOff15, 1: .buttonOn15],
-    16: [0: .buttonOff16, 1: .buttonOn16]
+    1:  [Input.off: .buttonOff1,  Input.on: .buttonOn1], // kHIDUsage_Button_1,
+    2:  [Input.off: .buttonOff2,  Input.on: .buttonOn2], // kHIDUsage_Button_2,
+    3:  [Input.off: .buttonOff3,  Input.on: .buttonOn3], // kHIDUsage_Button_3,
+    4:  [Input.off: .buttonOff4,  Input.on: .buttonOn4], // kHIDUsage_Button_4 exist but
+    5:  [Input.off: .buttonOff5,  Input.on: .buttonOn5], // kHIDUsage_Button_5 do not exist, so use Raw Int
+    6:  [Input.off: .buttonOff6,  Input.on: .buttonOn6],
+    7:  [Input.off: .buttonOff7,  Input.on: .buttonOn7],
+    8:  [Input.off: .buttonOff8,  Input.on: .buttonOn8],
+    9:  [Input.off: .buttonOff9,  Input.on: .buttonOn9],
+    10: [Input.off: .buttonOff10, Input.on: .buttonOn10],
+    11: [Input.off: .buttonOff11, Input.on: .buttonOn11],
+    12: [Input.off: .buttonOff12, Input.on: .buttonOn12],
+    13: [Input.off: .buttonOff13, Input.on: .buttonOn13],
+    14: [Input.off: .buttonOff14, Input.on: .buttonOn14],
+    15: [Input.off: .buttonOff15, Input.on: .buttonOn15],
+    16: [Input.off: .buttonOff16, Input.on: .buttonOn16]
   ],
   kHIDPage_GenericDesktop: [
-    kHIDUsage_GD_X: [0: .left, 127: .hNeutral, 255: .right],
-    kHIDUsage_GD_Y: [0: .up, 127: .vNeutral, 255: .down]
+    kHIDUsage_GD_X: [Input.axisPlus: .left, Input.axisNuetral: .hNeutral, Input.axisMinus: .right],
+    kHIDUsage_GD_Y: [Input.axisPlus: .up,   Input.axisNuetral: .vNeutral, Input.axisMinus: .down]
   ]
 ]
 
