d857dc24ff634b8d3592923dcbf57b428f957b67
gomachan_7 <tt@a-azarashi.jp>
2017-05-11 00:20:27 +0900

    Add getBar func to BMSData

--- harp/src/data/BMSData.swift
+++ harp/src/data/BMSData.swift
@@ -8,4 +8,10 @@ class BMSData {
   var barLength: Int {
     return bars.count
   }
+
+  func bar(atTick: Int) -> BMSBarData? {
+    return bars.first {
+      $0.startTick...($0.barTickCount + $0.startTick) ~= atTick
+    }
+  }
 }
