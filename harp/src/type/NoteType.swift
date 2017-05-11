94c3322a34c57b2775446b13267ad2833c4b42ed
gomachan_7 <tt@a-azarashi.jp>
2017-05-12 02:10:52 +0900

    Add new type to NoteType

--- harp/src/type/NoteType.swift
+++ harp/src/type/NoteType.swift
@@ -3,9 +3,12 @@ import Foundation
 enum NoteType: Int {
   case visible = 1
   case invisible = 2
-  case long = 3
+  case longStart = 3
+  case longEnd = 4
   
   // Note channel 11-69 will be note type
+  // All channel 5x-6x notes will be longStart(=3),
+  // some notes need to be re-define as longEnd.
   
   init?(channel: Int) {
     guard let side = SideType(channel: channel) else {
