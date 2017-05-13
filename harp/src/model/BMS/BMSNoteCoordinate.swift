4e2d0d2771537678d9fd80b7c9486cfcd6ea2233
gomachan_7 <tt@a-azarashi.jp>
2017-05-13 23:37:06 +0900

    Add BMSNoteCoordinate to calculate note scroll position

---
+++ harp/src/model/BMS/BMSNoteCoordinate.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+final class BMSNoteCoordinate {
+
+  private var hiSpeed: Double
+  private var basePixelPerTick: Double
+  
+  // MARK: - internal
+
+  init() {
+    hiSpeed = 1.0
+    basePixelPerTick = Config.BMS.defaultBarHeight / Double(Config.BMS.baseBarTick)
+  }
+
+  func setHiSpeed(_ hiSpeed: Double) {
+    self.hiSpeed = hiSpeed
+  }
+  
+  func coordYOfNote(tick: Int) -> Double {
+    let y = basePixelPerTick * hiSpeed * Double(tick)
+
+    return y
+  }
+}
