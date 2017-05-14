8416d82cc6f60bb79c35dd67b0238c595b0505a2
gomachan_7 <tt@a-azarashi.jp>
2017-05-14 14:26:00 +0900

    Fix to return scene from PlayScene

--- harp/src/scene/play/PlayController.swift
+++ harp/src/scene/play/PlayController.swift
@@ -1,5 +1,21 @@
 import SpriteKit
 
+protocol PlayControllerDelegate: class {
+  func playControllerWillExitScene()
+}
+
 class PlayController: Controller<PlayModel, PlayView> {
+  weak var delegate: PlayControllerDelegate?
   
+  // MARK: - private
+  
+  private func exitScene() {
+    delegate?.playControllerWillExitScene()
+  }
+  
+  override func rightMouseDown(with event: NSEvent) -> Bool {
+    exitScene()
+    
+    return true
+  }
 }
