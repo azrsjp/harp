faf6acd692459f10e17f8c55bae2eb62e1edf20f
gomachan_7 <tt@a-azarashi.jp>
2017-05-14 14:22:15 +0900

    Remove placeholder from PlayView and add reference to PlayModel

--- harp/src/scene/play/PlayView.swift
+++ harp/src/scene/play/PlayView.swift
@@ -1,13 +1,10 @@
 import SpriteKit
 
 class PlayView: View {
-  let myLabel = SKLabelNode(text: "Hello, World!")
+  
+  var model: PlayModel?
   
   override func constructView(sceneSize: CGSize) {
-    myLabel.fontColor = NSColor.black
-    myLabel.fontSize = 45
-    myLabel.position = CGPoint(x: sceneSize.width / 2, y: sceneSize.height / 2)
-    
-    addChild(myLabel)
+
   }
 }
