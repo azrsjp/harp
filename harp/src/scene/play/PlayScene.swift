87216bc959322d3c94f630ba827abfad6f40fbce
gomachan_7 <tt@a-azarashi.jp>
2017-05-14 14:26:00 +0900

    Fix to return scene from PlayScene

--- harp/src/scene/play/PlayScene.swift
+++ harp/src/scene/play/PlayScene.swift
@@ -1,13 +1,21 @@
 import SpriteKit
 
-class PlayScene: BasicScene<PlayModel, PlayView, PlayController> {
+class PlayScene: BasicScene<PlayModel, PlayView, PlayController>, PlayControllerDelegate {
   override func didMove(to view: SKView) {
     super.didMove(to: view)
+    
+    c.delegate = self
     
     backgroundColor = .white
   }
   
   func loadBMSFile(fullFilePath: String) {
     m.loadBMSFileAndIntialize(fullFilePath: fullFilePath)
+  }
+  
+  // MARK: - PlayControllerDelegate
+  
+  func playControllerWillExitScene() {
+    view?.presentScene(DebugMenuScene())
   }
 }
