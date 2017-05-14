18e5a77691629dcea1afc4ee8ff147cf03ea1ed0
gomachan_7 <tt@a-azarashi.jp>
2017-05-14 14:26:45 +0900

    Update PlayModel to load bms file

--- harp/src/scene/play/PlayController.swift
+++ harp/src/scene/play/PlayController.swift
@@ -1,4 +1,5 @@
 import SpriteKit
+import RxSwift
 
 protocol PlayControllerDelegate: class {
   func playControllerWillExitScene()
@@ -7,10 +8,47 @@ protocol PlayControllerDelegate: class {
 class PlayController: Controller<PlayModel, PlayView> {
   weak var delegate: PlayControllerDelegate?
   
+  private let disposeBag = DisposeBag()
+  private var notifier: InputEventNotifier<HIDInputConverterForInGame>?
+
+  override func initialize() {
+    view.model = model
+
+    notifier = InputEventNotifier(converter: HIDInputConverterForInGame())
+
+    guard let notifier = notifier else {
+      return
+    }
+
+    notifier.observable().subscribe(onNext: { [weak self] event in
+      guard let self_ = self else {
+        return
+      }
+      Logger.debug(event)
+      
+      self_.handleGameEvent(event)
+    }).addDisposableTo(disposeBag)
+  }
+  
   // MARK: - private
   
   private func exitScene() {
     delegate?.playControllerWillExitScene()
+  }
+  
+  private func handleGameEvent(_ event: GameEvent) {
+    switch event {
+    case .noteOn1: model.playKeySound(side: .player1, lane: .key1)
+    case .noteOn2: model.playKeySound(side: .player1, lane: .key2)
+    case .noteOn3: model.playKeySound(side: .player1, lane: .key3)
+    case .noteOn4: model.playKeySound(side: .player1, lane: .key4)
+    case .noteOn5: model.playKeySound(side: .player1, lane: .key5)
+    case .noteOn6: model.playKeySound(side: .player1, lane: .key6)
+    case .noteOn7: model.playKeySound(side: .player1, lane: .key7)
+    case .scrachLeft: model.playKeySound(side: .player1, lane: .scratch)
+    default:
+      break
+    }
   }
   
   override func rightMouseDown(with event: NSEvent) -> Bool {
--- harp/src/scene/play/PlayModel.swift
+++ harp/src/scene/play/PlayModel.swift
@@ -1,4 +1,82 @@
 import Foundation
+import RxSwift
 
 class PlayModel: Model {
+  private let thread = ElapsedBasedThread()
+  private let disposeBag = DisposeBag()
+
+  private var soundPlayer: SoundPlayer<Int>?
+  private var bmsData: BMSData?
+  private var tick: BMSTick?
+  private let coordinate = BMSNoteCoordinate()
+  private var sound: BMSSound?
+
+  deinit {
+    thread.cancel()
+  }
+
+  // MARK: - internal
+
+  func loadBMSFileAndIntialize(fullFilePath: String) {
+    guard
+      let lines = String(unknownEncodingContentsOfFile: fullFilePath),
+      let playerFactory = SoundPlayerFactory.shared,
+      let dirPath = fullFilePath.getFileDirPathIfAvailable() else {
+        return
+    }
+
+    BMSParser.parse(contentsOfBMS: lines)
+      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+      .do(onNext: { [weak self] in self?.bmsData = $0 })
+      .map { $0.header.wav.mapValue { dirPath + "/" + $0 } }
+      .flatMap { playerFactory.makePlayer(keyAndFileFullPath: $0) }
+      .observeOn(MainScheduler.instance)
+      .subscribe(onNext: { [weak self] player in
+        guard let self_ = self else {
+          return
+        }
+
+        Logger.info("Resources is loaded")
+
+        self_.soundPlayer = player
+        self_.tick = BMSTick(data: self_.bmsData!)
+        self_.sound = BMSSound(data: self_.bmsData!)
+        self_.sound!.updateKeyAssignAt(tick: 0)
+
+        self_.startBMSPlayer()
+      }).addDisposableTo(disposeBag)
+  }
+
+  func playKeySound(side: SideType, lane: LaneType) {
+    guard let key = sound?.keySoundKeyToPlay(side: side, lane: lane) else {
+      return
+    }
+
+    soundPlayer?.play(forKey: key)
+  }
+
+  // MARK: - private
+
+  private func startBMSPlayer() {
+
+    thread.start { [weak self] elapsed in
+      guard let self_ = self else {
+        return
+      }
+
+      self_.update(elapsed)
+    }
+  }
+
+  private func update(_ elapsed: Double) {
+    let currentTick = tick!.tickAt(elapsedSec: elapsed)
+    
+    sound!.updateKeyAssignAt(tick: currentTick, lookAheadTickCount: 1000)
+    
+    sound!
+      .soundKeysToPlayAt(tick: currentTick)
+      .forEach { [weak self] key in
+        self?.soundPlayer?.play(forKey: key)
+    }
+  }
 }
