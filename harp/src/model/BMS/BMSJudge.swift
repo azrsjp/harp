f4521eb5cb3c88324ba55f7da4d32817f13ee5b8
gomachan_7 <tt@a-azarashi.jp>
2017-05-23 02:12:39 +0900

    :recycle: Refactor BMSJudge contains BMSNoteState to out notes state

--- harp/src/model/BMS/BMSJudge.swift
+++ harp/src/model/BMS/BMSJudge.swift
@@ -1,43 +1,15 @@
 import Foundation
 
-fileprivate let eventToLane: [GameEvent: LaneType]
-  = [
-    GameEvent.noteOn1: LaneType.key1,
-    GameEvent.noteOff1: LaneType.key1,
-    GameEvent.noteOn2: LaneType.key2,
-    GameEvent.noteOff2: LaneType.key2,
-    GameEvent.noteOn3: LaneType.key3,
-    GameEvent.noteOff3: LaneType.key3,
-    GameEvent.noteOn4: LaneType.key4,
-    GameEvent.noteOff4: LaneType.key4,
-    GameEvent.noteOn5: LaneType.key5,
-    GameEvent.noteOff5: LaneType.key5,
-    GameEvent.noteOn6: LaneType.key6,
-    GameEvent.noteOff6: LaneType.key6,
-    GameEvent.noteOn7: LaneType.key7,
-    GameEvent.noteOff7: LaneType.key7,
-    GameEvent.scrachLeft: LaneType.scratch,
-    GameEvent.scrachRight: LaneType.scratch,
-    GameEvent.scrachEnd: LaneType.scratch
-  ]
-
 final class BMSJudge {
-
-  static private let comobChainer: [Judge] = [.perfectGreat, .great, .good]
   private let lock = NSLock()
-
+  private let notes: BMSNotesState
   private let tick: BMSTick
-  private var notYetJudgedNotes = [BMSBarNoteData]()
-  private var judgedData = [BMSBarNoteData: Judge]()
-  private var inLongNoteState = [BMSNoteTraitData]()
-
   private var lastJudge = Judge.notYet
   private var combo = 0
 
-  init(data: BMSData) {
-    tick = BMSTick(data: data)
-
-    notYetJudgedNotes = data.score.notes
+  init(notes: BMSNotesState, tick: BMSTick) {
+    self.tick = tick
+    self.notes = notes
   }
 
   // MARK: - internal
@@ -45,69 +17,52 @@ final class BMSJudge {
   func judge(event: GameEvent, elapsed: Double) {
     lock.lock()
     defer { lock.unlock() }
-    
-    guard let lane = eventToLane[event] else {
+
+    guard let (lane, side) = event.sideAndLane else {
       return
     }
 
-    guard let judgeTargetIndex = getJudgeTargetNoteIndex(lane: lane, elapsed: elapsed) else {
+    guard let noteToJudge = findNoteToJudge(lane: lane, side: side, elapsed: elapsed) else {
 
-      // If long note is active in the lane, key is released early, so judge long end note as poor.
-      if let startLongNoteTrait = inLongNoteState.first(where: { $0.lane == lane }) {
-        breakLongNoteByEarlyRelaseKey(trait: startLongNoteTrait)
+      // If long note is active in the lane, judge long end note as poor by early key release.
+      if !event.isKeyDownEvent,
+        notes.breakLongNoteStateIfNeeded(side: side, lane: lane, tick: tick.tickAt(elapsedSec: elapsed)) {
+        updateComboAndCurrentJudge(judge: .negativePoor)
       }
 
       return
     }
 
-    let judgeTargetNote = notYetJudgedNotes.remove(at: judgeTargetIndex)
-    let judged = getJudged(elapsed: elapsed, justTiming: tick.elapsedAt(tick: judgeTargetNote.tick))
-
-    updateComboAndCurrentJudge(judge: judged)
-
-    switch event {
-    case .noteOn1: fallthrough
-    case .noteOn2: fallthrough
-    case .noteOn3: fallthrough
-    case .noteOn4: fallthrough
-    case .noteOn5: fallthrough
-    case .noteOn6: fallthrough
-    case .noteOn7: fallthrough
-    case .scrachLeft: fallthrough
-    case .scrachRight:
-      judgedData[judgeTargetNote] = judged
-      startLongNoteIfNeeded(note: judgeTargetNote)
-    case .noteOff1: fallthrough
-    case .noteOff2: fallthrough
-    case .noteOff3: fallthrough
-    case .noteOff4: fallthrough
-    case .noteOff5: fallthrough
-    case .noteOff6: fallthrough
-    case .noteOff7:
-      judgedData[judgeTargetNote] = judged
-      breakLongNoteIfNeeded(note: judgeTargetNote)
-    default: break
+    guard event.isKeyDownEvent else {
+      return
     }
+
+    let judgeResult = getJudged(elapsed: elapsed, justTiming: tick.elapsedAt(tick: noteToJudge.tick))
+
+    notes.markNoteAsDead(noteToJudge, judge: judgeResult)
+    notes.markLongStartNoteActive(noteToJudge)
+    updateComboAndCurrentJudge(judge: judgeResult)
   }
 
   func judgeMissedNotesAt(elapsed: Double) {
     lock.lock()
     defer { lock.unlock() }
 
-    let missedNotes
-      = notYetJudgedNotes.filter { isInMissRange(elpased: elapsed,
-                                                 justTiming: tick.elapsedAt(tick: $0.tick)) }
+    let missedNotes = notes.alive.prefix { isInMissRange(elpased: elapsed,
+                                                         justTiming: tick.elapsedAt(tick: $0.tick)) }
 
-    guard missedNotes.count > 0 else {
-      return
-    }
+    // If only inActive long note judges, continue combo.
+    var shouldRecordJudged = false
 
     missedNotes.forEach {
-      judgedData[$0] = .negativePoor
-      breakLongNoteIfNeeded(note: $0)
+      let judgeSuccess = notes.markNoteAsDead($0, judge: .negativePoor)
+
+      shouldRecordJudged = judgeSuccess || shouldRecordJudged
     }
 
-    updateComboAndCurrentJudge(judge: .negativePoor)
+    if shouldRecordJudged {
+      updateComboAndCurrentJudge(judge: .negativePoor)
+    }
   }
 
   func getLastJudge() -> Judge {
@@ -119,32 +74,39 @@ final class BMSJudge {
   }
 
   func getJudgeOf(note: BMSBarNoteData) -> Judge {
-    guard let judge = judgedData[note] else {
-      return .notYet
-    }
-
-    return judge
+    return notes.judgeOf(note: note)
   }
 
   // MARK: - private
 
-  private func getJudgeTargetNoteIndex(lane: LaneType, elapsed: Double) -> Array<BMSBarNoteData>.Index? {
-    guard let judgeTargetIndex
-      = notYetJudgedNotes.index(where: {
+  private func findNoteToJudge(lane: LaneType, side: SideType, elapsed: Double) -> BMSBarNoteData? {
+    guard let targetNote = notes.alive.first(where: {
         $0.trait.lane == lane &&
-        $0.trait.type != .invisible &&
+        $0.trait.side == side &&
         isInJudgeableRange(elpased: elapsed, justTiming: tick.elapsedAt(tick: $0.tick)) }) else {
         return nil
     }
 
-    return judgeTargetIndex
+    return targetNote
+  }
+
+  private func updateComboAndCurrentJudge(judge: Judge) {
+    if [.perfectGreat, .great, .good].contains(judge) {
+      combo += 1
+    } else if [.positivePoor].contains(judge) {
+      // do nothing for combo
+    } else {
+      combo = 0
+    }
+
+    lastJudge = judge
   }
 
   private func isInJudgeableRange(elpased: Double, justTiming: Double) -> Bool {
     let isOverJudgeLine = elpased - justTiming > 0.0
 
     return isOverJudgeLine ?
-    isInBadRange(elpased: elpased, justTiming: justTiming):
+    isInBadRange(elpased: elpased, justTiming: justTiming) :
       isInPoorRange(elpased: elpased, justTiming: justTiming)
   }
 
@@ -189,61 +151,5 @@ final class BMSJudge {
     }
 
     return .negativePoor
-  }
-
-  private func updateComboAndCurrentJudge(judge: Judge) {
-    if BMSJudge.comobChainer.contains(judge) {
-      combo += 1
-    } else {
-      combo = 0
-    }
-
-    lastJudge = judge
-  }
-
-  private func startLongNoteIfNeeded(note: BMSBarNoteData) {
-    guard note.trait.type == .longStart else {
-      return
-    }
-
-    if !inLongNoteState.contains(where: { $0 == note.trait }) {
-      inLongNoteState.append(note.trait)
-    }
-  }
-
-  private func breakLongNoteIfNeeded(note: BMSBarNoteData) {
-    guard note.trait.type == .longEnd else {
-      return
-    }
-
-    let longNoteStartTrait
-      = BMSNoteTraitData(side: note.trait.side, lane: note.trait.lane, type: .longStart)
-
-    if let index = inLongNoteState.index(where: { $0 == longNoteStartTrait }) {
-      inLongNoteState.remove(at: index)
-    }
-  }
-
-  private func breakLongNoteByEarlyRelaseKey(trait: BMSNoteTraitData) {
-    guard trait.type == .longStart else {
-      return
-    }
-
-    if let index = inLongNoteState.index(where: { $0 == trait }) {
-      inLongNoteState.remove(at: index)
-    }
-
-    let longNoteEndTrait
-      = BMSNoteTraitData(side: trait.side, lane: trait.lane, type: .longEnd)
-
-    guard let nextLongNoteEndIndex
-      = notYetJudgedNotes.index(where: { $0.trait == longNoteEndTrait }) else {
-        return
-    }
-
-    let nextLongNoteEnd = notYetJudgedNotes.remove(at: nextLongNoteEndIndex)
-    judgedData[nextLongNoteEnd] = .negativePoor
-
-    updateComboAndCurrentJudge(judge: .negativePoor)
   }
 }
