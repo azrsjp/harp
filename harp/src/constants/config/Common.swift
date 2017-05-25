81cc574f15cf0db932434ff8367833d0500b7cee
gomachan_7 <tt@a-azarashi.jp>
2017-05-26 01:39:55 +0900

    Down window resolution

--- harp/src/constants/config/Common.swift
+++ harp/src/constants/config/Common.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct Config {
   struct Common {
-    static let defaultWidth: CGFloat = 1920.0
-    static let defaultHeight: CGFloat = 1080.0
+    static let defaultWidth: CGFloat = 1280.0
+    static let defaultHeight: CGFloat = 720.0
     static let defaultWindowSize = CGSize(width: defaultWidth, height: defaultHeight)
     
     static let defaultFontName = "Helvetica"
@@ -13,8 +13,8 @@ struct Config {
     static let resolution: Int = 480 * 14
     static let baseBarTick: Int = resolution * 4
     static let keyForRest: String = "00"
-    static let defaultBarHeight: Double = 480.0
-    static let defaultLaneHeight: Double = 720.0
+    static let defaultBarHeight: Double = 320.0
+    static let defaultLaneHeight: Double = 578.0
     
     static let pGreatRange: Double = 0.020
     static let greatRange: Double = 0.040
