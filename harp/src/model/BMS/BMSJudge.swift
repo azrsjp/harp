7a607cb429323339c5403f148b5c5b5b228f05e3
gomachan_7 <tt@a-azarashi.jp>
2017-05-26 01:37:41 +0900

    Judge normal note and long end note separately

--- harp/src/model/BMS/BMSJudge.swift
+++ harp/src/model/BMS/BMSJudge.swift
@@ -4,12 +4,28 @@ final class BMSJudge {
   private let lock = NSLock()
   private let notes: BMSNotesState
   private let tick: BMSTick
+  private let normalRange: BMSJudgeableRange
+  private let longEndRange: BMSJudgeableRange
   private var lastJudge = Judge.notYet
   private var combo = 0
 
   init(notes: BMSNotesState, tick: BMSTick) {
     self.tick = tick
     self.notes = notes
+    
+    // JudgeRange is 2way, normal and longStart, or longEnd note
+    self.normalRange = BMSJudgeableRange(pGreatRange: Config.BMS.pGreatRange,
+                                         greatRange: Config.BMS.greatRange,
+                                         goodRange: Config.BMS.goodRange,
+                                         badRange: Config.BMS.badRange,
+                                         poorRange: Config.BMS.badRange)
+
+    let additionaRange = 0.08
+    self.longEndRange = BMSJudgeableRange(pGreatRange: Config.BMS.pGreatRange + additionaRange,
+                                         greatRange: Config.BMS.greatRange + additionaRange,
+                                         goodRange: Config.BMS.goodRange + additionaRange,
+                                         badRange: Config.BMS.badRange + additionaRange,
+                                         poorRange: Config.BMS.badRange)
   }
 
   // MARK: - internal
@@ -22,36 +38,22 @@ final class BMSJudge {
       return
     }
 
-    guard let noteToJudge = findNoteToJudge(lane: lane, side: side, elapsed: elapsed) else {
-
-      // If long note is active in the lane, judge long end note as poor by early key release.
-      if !event.isKeyDownEvent,
-        notes.breakLongNoteStateIfNeeded(side: side, lane: lane, tick: tick.tickAt(elapsedSec: elapsed)) {
-        updateComboAndCurrentJudge(judge: .negativePoor)
-      }
-
-      return
+    if event.isKeyDownEvent {
+      judgeOnKeyDown(lane: lane, side: side, elapsed: elapsed)
+    } else {
+      judgeOnKeyUp(lane: lane, side: side, elapsed: elapsed)
     }
-
-    guard event.isKeyDownEvent ||
-      (!event.isKeyDownEvent && noteToJudge.trait.type == .longEnd) else {
-      return
-    }
-
-    let judgeResult = getJudged(elapsed: elapsed, justTiming: tick.elapsedAt(tick: noteToJudge.tick))
-
-    notes.markNoteAsDead(noteToJudge, judge: judgeResult)
-    notes.markLongStartNoteActive(noteToJudge)
-    updateComboAndCurrentJudge(judge: judgeResult)
   }
 
   func judgeMissedNotesAt(elapsed: Double) {
     lock.lock()
     defer { lock.unlock() }
 
-    let missedNotes = notes.alive.prefix { isInMissRange(elpased: elapsed,
-                                                         justTiming: tick.elapsedAt(tick: $0.tick)) }
-    
+    let missedNotes = notes.alive.filter {
+      ($0.trait.type != .longEnd && normalRange.isInMissRange(elpased: elapsed, justTiming: tick.elapsedAt(tick: $0.tick))) ||
+      ($0.trait.type == .longEnd && longEndRange.isInMissRange(elpased: elapsed, justTiming: tick.elapsedAt(tick: $0.tick)))
+    }
+
     // On coming inActive(already dead) long note end judges, continue combo.
     var shouldBreakCombo = false
 
@@ -80,15 +82,59 @@ final class BMSJudge {
 
   // MARK: - private
 
-  private func findNoteToJudge(lane: LaneType, side: SideType, elapsed: Double) -> BMSBarNoteData? {
+  private func findNoteToJudge(lane: LaneType, side: SideType,
+                               elapsed: Double, range: BMSJudgeableRange) -> BMSBarNoteData? {
+    let laneHash = BMSNoteTraitData.laneHash(lane: lane, side: side)
+
     guard let targetNote = notes.alive.first(where: {
-        $0.trait.lane == lane &&
-        $0.trait.side == side &&
-        isInJudgeableRange(elpased: elapsed, justTiming: tick.elapsedAt(tick: $0.tick)) }) else {
+        $0.trait.laneHash == laneHash &&
+        range.isInJudgeableRange(elpased: elapsed, justTiming: tick.elapsedAt(tick: $0.tick)) }) else {
         return nil
     }
 
     return targetNote
+  }
+  
+  private func judgeOnKeyDown(lane: LaneType, side: SideType, elapsed: Double) {
+    guard let noteToJudge = findNoteToJudge(lane: lane, side: side, elapsed: elapsed, range: normalRange) else {
+      return
+    }
+    
+    let judgeResult = normalRange.getJudged(elapsed: elapsed, justTiming: tick.elapsedAt(tick: noteToJudge.tick))
+    
+    notes.markNoteAsDead(noteToJudge, judge: judgeResult)
+    notes.markLongStartNoteActive(noteToJudge)
+    updateComboAndCurrentJudge(judge: judgeResult)
+  }
+  
+  private func judgeOnKeyUp(lane: LaneType, side: SideType, elapsed: Double) {
+    guard let noteToJudge = findNoteToJudge(lane: lane, side: side, elapsed: elapsed, range: longEndRange) else {
+      
+      // If long note is active in the lane, judge long end note as poor by early key release.
+      if notes.breakLongNoteStateIfNeeded(side: side, lane: lane, tick: tick.tickAt(elapsedSec: elapsed)) {
+        updateComboAndCurrentJudge(judge: .negativePoor)
+      }
+      return
+    }
+    
+    guard noteToJudge.trait.type == .longEnd else {
+      return
+    }
+    
+    var judgeResult = longEndRange.getJudged(elapsed: elapsed, justTiming: tick.elapsedAt(tick: noteToJudge.tick))
+    
+    // overwrite judge to break combo
+    if judgeResult == .positivePoor {
+      judgeResult == .negativePoor
+    }
+    
+    // Indicate as broken long note (set long note bar inactive)
+    if [.bad, .negativePoor].contains(judgeResult) {
+      notes.breakLongNoteStateIfNeeded(side: side, lane: lane, tick: tick.tickAt(elapsedSec: elapsed))
+    }
+    
+    updateComboAndCurrentJudge(judge: judgeResult)
+    notes.markNoteAsDead(noteToJudge, judge: judgeResult)
   }
 
   private func updateComboAndCurrentJudge(judge: Judge) {
@@ -101,56 +147,5 @@ final class BMSJudge {
     }
 
     lastJudge = judge
-  }
-
-  private func isInJudgeableRange(elpased: Double, justTiming: Double) -> Bool {
-    let isOverJudgeLine = elpased - justTiming > 0.0
-
-    return isOverJudgeLine ?
-    isInBadRange(elpased: elpased, justTiming: justTiming) :
-      isInPoorRange(elpased: elpased, justTiming: justTiming)
-  }
-
-  private func isInMissRange(elpased: Double, justTiming: Double) -> Bool {
-    let diff = elpased - justTiming
-
-    guard diff > 0.0 else {
-      return false
-    }
-
-    return diff > Config.BMS.badRange
-  }
-
-  private func isInPerfectGreatRange(elpased: Double, justTiming: Double) -> Bool {
-    return abs(elpased - justTiming) <= Config.BMS.pGreatRange
-  }
-
-  private func isInGreatRange(elpased: Double, justTiming: Double) -> Bool {
-    return abs(elpased - justTiming) <= Config.BMS.greatRange
-  }
-
-  private func isInGoodRange(elpased: Double, justTiming: Double) -> Bool {
-    return abs(elpased - justTiming) <= Config.BMS.goodRange
-  }
-
-  private func isInBadRange(elpased: Double, justTiming: Double) -> Bool {
-    return abs(elpased - justTiming) <= Config.BMS.badRange
-  }
-
-  private func isInPoorRange(elpased: Double, justTiming: Double) -> Bool {
-    return abs(elpased - justTiming) <= Config.BMS.poorRange
-  }
-
-  private func getJudged(elapsed: Double, justTiming: Double) -> Judge {
-    switch true {
-    case isInPerfectGreatRange(elpased: elapsed, justTiming: justTiming): return .perfectGreat
-    case isInGreatRange(elpased: elapsed, justTiming: justTiming): return .great
-    case isInGoodRange(elpased: elapsed, justTiming: justTiming): return .good
-    case isInBadRange(elpased: elapsed, justTiming: justTiming): return .bad
-    case isInPoorRange(elpased: elapsed, justTiming: justTiming): return .positivePoor
-    default: break
-    }
-
-    return .negativePoor
   }
 }
