1ddb9d36f988425d1bc3bab9131bb7d7973d84d6
gomachan_7 <tt@a-azarashi.jp>
2017-05-27 19:01:10 +0900

    Add SKPlayField

---
+++ harp/src/ui/SKPlayField.swift
@@ -0,0 +1,183 @@
+import SpriteKit
+
+fileprivate let laneSeparatorSize = CGSize(width: 2.0, height: CGFloat(Config.BMS.defaultLaneHeight))
+fileprivate let barLineZPosition: CGFloat = 1.0
+fileprivate let judgeLineZPosition: CGFloat = 2.0
+fileprivate let noteZPosition: CGFloat = 3.0
+
+final class SKPlayField: SKSpriteNode {
+
+  private var lanes = [LaneType: SKLane]()
+  private let laneCover = SKSpriteNode()
+  private let judgeLine = SKSpriteNode()
+  private let lift = SKSpriteNode()
+  
+  private var barLines = [SKSpriteNode]()
+  private var normalNotes = [Int: SKNote]()
+  private var longNotes = [Int: SKNote]()
+
+  init(type: SideType) {
+    let width: CGFloat
+      = SKNote.NoteType.white.size.width * CGFloat(4.0) +
+        SKNote.NoteType.black.size.width * CGFloat(3.0) +
+        SKNote.NoteType.scratch.size.width * CGFloat(1.0) +
+        laneSeparatorSize.width * CGFloat(6.0)
+
+    super.init(texture: nil, color: .black,
+               size: CGSize(width: width, height: CGFloat(Config.BMS.defaultLaneHeight)))
+    
+    judgeLine.size = CGSize(width: width, height: 5.0)
+    judgeLine.color = NSColor(named: .judgeLine)
+    judgeLine.anchorPoint = CGPoint.zero
+    judgeLine.position = CGPoint(x: 0.0, y: 0.0)
+    judgeLine.zPosition = judgeLineZPosition
+    addChild(judgeLine)
+    
+    setupLanes(laneOrder: (type == .player1) ?
+      [.scratch, .key1, .key2, .key3, .key4, .key5, .key6, .key7] :
+      [.key1, .key2, .key3, .key4, .key5, .key6, .key7, .scratch])
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - internal
+  
+  func updateBarLineCoords(_ coords: [BMSBarLineCoordData]) {
+    let shortage = coords.count - barLines.count
+    if shortage > 0 {
+      let addition = [SKSpriteNode](repeating: barLineFactory(), count: shortage)
+      addition.forEach { addChild($0) }
+      barLines.append(contentsOf: addition)
+    }
+
+    barLines.forEach {
+      $0.isHidden = true
+    }
+
+    zip(coords, barLines).forEach {
+      $1.position = CGPoint(x: 0.0, y: $0.offsetY)
+      $1.isHidden = false
+    }
+  }
+  
+  func updateNormalNoteCoords(_ coords: [BMSBasicNoteCoord]) {
+    updateNoteCoords(coords, targetNoteGroup: &normalNotes)
+  }
+  
+  func updateLongNoteCoords(_ coords: [BMSLongNoteCoordData]) {
+    updateNoteCoords(coords, targetNoteGroup: &longNotes)
+
+    // Adjust long note feature
+    coords.forEach {
+      guard let note = longNotes[$0.noteId] else {
+        return
+      }
+      
+      note.setLongActive($0.isActive)
+      note.setLonglength($0.length)
+    }
+  }
+  
+  func setLaneCoverValue(_ value: Int) {
+  
+  }
+  
+  func setLiftValue(_ value: Int) {
+
+  }
+  
+  // MARK: - private
+  
+  private func updateNoteCoords(_ coords: [BMSBasicNoteCoord],
+                                targetNoteGroup: inout [Int: SKNote]) {
+    // Remove Notes(already out of fileld) Phase
+    targetNoteGroup
+      .filter { note in !coords.contains { $0.noteId == note.key } }
+      .forEach {
+        $0.value.removeFromParent()
+        targetNoteGroup.removeValue(forKey: $0.key)
+    }
+    
+    // Place Notes Phase
+    coords.forEach {
+      if let note = targetNoteGroup[$0.noteId] {
+        placeNote(coord: $0, note: note)
+      } else {
+        guard $0.judge == .notYet else { return }
+        
+        let newNote = noteFactory(lane: $0.trait.lane)
+        
+        targetNoteGroup[$0.noteId] = newNote
+        placeNote(coord: $0, note: newNote)
+      }
+    }
+  }
+
+  private func placeNote(coord: BMSBasicNoteCoord, note: SKNote) {
+    if note.parent == nil, let lane = lanes[coord.trait.lane] {
+      lane.addChild(note)
+    }
+    
+    note.position = CGPoint(x: 0.0, y: coord.offsetY)
+  }
+  
+  private func setupLanes(laneOrder: [LaneType]) {
+    var laneX: CGFloat = 0.0
+
+    laneOrder.enumerated().forEach { i, laneType in
+      let lane = SKLane(noteTypeToHold: SKNote.NoteType.fromLaneType(laneType))
+      lane.anchorPoint = CGPoint.zero
+      lane.position = CGPoint(x: laneX, y: 0.0)
+      addChild(lane)
+      
+      lanes[laneType] = lane
+
+      if i < laneOrder.count - 1 {
+        let laneSeparator = SKSpriteNode(color: NSColor(named: .laneBorder), size: laneSeparatorSize)
+        laneSeparator.anchorPoint = CGPoint.zero
+        laneSeparator.position = CGPoint(x: laneX + lane.width, y: 0.0)
+        addChild(laneSeparator)
+        
+        laneX += lane.width + laneSeparatorSize.width
+      }
+    }
+  }
+  
+  private func barLineFactory() -> SKSpriteNode {
+    let line = SKSpriteNode()
+    line.color = NSColor(named: .barBorder)
+    line.size = CGSize(width: self.size.width, height: 2.0)
+    line.anchorPoint = CGPoint.zero
+    line.zPosition = barLineZPosition
+    
+    return line
+  }
+  
+  private func noteFactory(lane: LaneType) -> SKNote {
+    let note = SKNote(type: SKNote.NoteType.fromLaneType(lane))
+    note.anchorPoint = CGPoint.zero
+    note.zPosition = noteZPosition
+    
+    return note
+  }
+}
+
+fileprivate final class SKLane: SKSpriteNode {
+  let width: CGFloat
+
+  init(noteTypeToHold: SKNote.NoteType) {
+    width = noteTypeToHold.size.width
+    
+    let size = CGSize(width: width, height: CGFloat(Config.BMS.defaultLaneHeight))
+    let color: NSColor = noteTypeToHold == .white ?
+      NSColor(named: .laneBgGray) : NSColor(named: .laneBgBlack)
+
+    super.init(texture: nil, color: color, size: size)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
