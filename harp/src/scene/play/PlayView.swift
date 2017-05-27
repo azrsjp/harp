81d8c4cba3c15ec96ee69492043bab512009c7b2
gomachan_7 <tt@a-azarashi.jp>
2017-05-27 19:02:33 +0900

    Update PlayView to use SKPlayField

--- harp/src/scene/play/PlayView.swift
+++ harp/src/scene/play/PlayView.swift
@@ -3,8 +3,37 @@ import SpriteKit
 class PlayView: View {
   
   var model: PlayModel?
+
+  private var field = SKPlayField(type: .player1)
+  private let combo = SKLabelNode()
+  private let judge = SKLabelNode()
   
   override func constructView(sceneSize: CGSize) {
+    field.anchorPoint = CGPoint.zero
+    field.position = CGPoint(x: 15.0, y: sceneSize.height - field.size.height)
+    addChild(field)
+  
+    combo.position = CGPoint(x: 200.0, y: 140.0)
+    combo.fontColor = .white
+    combo.horizontalAlignmentMode = .left
+    field.addChild(combo)
 
+    judge.position = CGPoint(x: 200.0, y: 140.0)
+    judge.fontColor = .white
+    judge.horizontalAlignmentMode = .right
+    field.addChild(judge)
+  }
+
+  override func update(_ currentTime: TimeInterval) {
+    guard let model = model, let coords = model.currentCoordData() else {
+      return
+    }
+
+    field.updateBarLineCoords(coords.barLines)
+    field.updateNormalNoteCoords(coords.notes)
+    field.updateLongNoteCoords(coords.longNotes)
+
+    combo.text = String(coords.combo)
+    judge.text = String(coords.judge)
   }
 }
